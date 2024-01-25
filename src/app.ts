44613c33974ebd11b70c97533a323c6da9725885
furqanrupom <furqanrupom978@gmail.com>
2024-01-26 02:15:57 +0600

    set up the server side

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application } from "express"
+import cors from "cors"
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req,res) => {
+   res.send('GardenGlance is running')
+})
+
+
+export default app;
