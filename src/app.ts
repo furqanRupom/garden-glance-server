33700bcd6a40f1993db6aa34e37e331521212e43
furqanrupom <furqanrupom978@gmail.com>
2024-01-28 03:43:42 +0600

    make some changes

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,18 @@
-import express, { Application } from "express"
-import cors from "cors"
-import router from "./app/routes/route";
-import globalErrorHandler from "./app/middleware/globalErrorHandler";
+import express, { Application } from 'express';
+import cors from 'cors';
+import router from './app/routes/route';
+import globalErrorHandler from './app/middleware/globalErrorHandler';
 
 const app: Application = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 
-app.use('/',router);
+app.use('/', router);
 
 app.use(globalErrorHandler);
-app.get('/', (req,res) => {
-   res.send('GardenGlance is running');
-})
-
+app.get('/', (req, res) => {
+  res.send('GardenGlance is running');
+});
 
 export default app;
